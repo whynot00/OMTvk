8d0e246a660fdf98e1ee9bab50f242889589ac59
Roman <whynot2288@gmail.com>
2024-05-25 17:04:23 +0300

    Скомпилирован .exe файл.

--- additional_functions.py
+++
@@ -1,9 +0,0 @@
-import time
-
-def time_of_function(function):
-    def wrapped(*args):
-        start_time = time.time()
-        res = function(*args)
-        print("\nВремя выполнения:",round(time.time() - start_time, 2), "сек.")
-        return res
-    return wrapped
